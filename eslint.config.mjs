54f859447dc67fffaee2e4cad17f345d92362742
jinweizhi <jinweizhi0327@outlook.com>
2025-04-13 18:57:49 +0800

    feat: 初始化配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig } from 'eslint/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,58 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+console.log(1234, compat.config);
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+      'eslint-config-prettier',
+    ],
+    plugins: ['prettier'],
+    rules: {
+      'import/order': [
+        'error',
+        {
+          //按照分组顺序进行排序
+          groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'internal', 'object', 'type'],
+          //通过路径自定义分组
+          pathGroups: [
+            {
+              pattern: 'react*',
+              group: 'builtin',
+              position: 'before',
+            },
+            {
+              pattern: '@/components/**',
+              group: 'parent',
+              position: 'before',
+            },
+            {
+              pattern: '@/utils/**',
+              group: 'parent',
+              position: 'after',
+            },
+            {
+              pattern: '@/apis/**',
+              group: 'parent',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'always', //每个分组之间换行
+          //根据字母顺序对每个组内的顺序进行排序
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  }),
 ];
 
-export default eslintConfig;
+export default defineConfig(eslintConfig);
